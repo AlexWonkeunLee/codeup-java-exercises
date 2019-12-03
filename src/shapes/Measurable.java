cdb78c86e99a6fb174e029b2a5f18c0c1aa61e18
AlexWonkeunLee <AlexWonkeunLee@gmail.com>
2019-12-03 10:27:44 -0600

    fix

--- src/shapes/Measurable.java
+++ src/shapes/Measurable.java
@@ -3,4 +3,6 @@ package shapes;
 public interface Measurable {
     double getPerimeter();
     double getArea();
+    double getLength();
+    double getWidth();
 }
--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -6,8 +6,12 @@ public class ShapesTest {
         myShape = new Square(5);
         System.out.println(myShape.getArea());
         System.out.println(myShape.getPerimeter());
+        System.out.println(myShape.getLength());
+        System.out.println(myShape.getWidth());
         myShape = new Rectangle(5,4);
         System.out.println(myShape.getArea());
         System.out.println(myShape.getPerimeter());
+        System.out.println(myShape.getLength());
+        System.out.println(myShape.getWidth());
     }
 }
